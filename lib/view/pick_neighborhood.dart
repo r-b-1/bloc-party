2fb7c8d6998ee13fe9c630dd3fc3c607236cb0db
Shane Toutges <toutg011@d.umn.edu>
2025-10-22 12:55:13 -0700

    added list of items that is displayed

---
+++ lib/view/pick_neighborhood.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+//temp
+final List<String> available_neighborhoods = List<String>.generate(100, (i) => '$i');
+//temp
+
+class PickNeighborhoodView extends StatelessWidget {
+  const PickNeighborhoodView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Neighborhood Selection'),
+      ),
+      body: ListView.builder(
+        
+        itemCount: available_neighborhoods.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 114, 26, 255),
+              child: Text(available_neighborhoods[index]),
+              
+            ),
+
+            title: Text('Neighborhood ${available_neighborhoods[index]}'),
+            subtitle: Text('Description'),
+            trailing: GestureDetector(
+              onTap: () {
+                //do neighborhood change/join logic
+
+                //temp
+                Navigator.pushNamed(context, '/home');  //no animation on the button, so I put this here to show it works
+                //temp
+              },
+              child: Icon(Icons.more_vert),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
