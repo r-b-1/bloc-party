75e4f6e3c8c633eeeae880ac5c17f5ddab5b8fa4
Cole Juran <juran015@d.umn.edu>
2025-10-30 14:37:56 -0500

    added chat class and its firebase integration

--- lib/model/chat_model.dart
+++ lib/model/chat_model.dart
@@ -1,1 +1,39 @@
+import 'package:blocparty/model/message_model.dart';
+import 'package:blocparty/model/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Chat{
+  final String name;
+  final List<AddUser> members;
+  final List<Message> messages;
+
+  Chat({
+    required this.name,
+    required this.members,
+    required this.messages
+  });
+
+  factory Chat.fromFirestore(DocumentSnapshot<Map<String, dynamic>> doc) {
+    if (!doc.exists) {
+      throw Exception('Document does not exist');
+    }
+    final data = doc.data();
+    if (data == null) {
+      throw Exception('Document data is null');
+    }
+
+    return Chat(
+      name: data['name'] ?? '',
+      members: data['members'] ?? '',
+      messages: data['messages'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'members': members,
+      'messages': messages,
+    };
+  }
+}
