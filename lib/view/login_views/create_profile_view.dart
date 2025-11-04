c2f7877b7419f0cb278b195448572d8281df4658
Carson Scheich <146121287+CarsonScheich@users.noreply.github.com>
2025-11-04 14:14:46 -0600

    getting organization in folders

--- lib/view/login_views/create_profile_view.dart
+++ lib/view/login_views/create_profile_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go_router/go_router.dart';
-import 'package:blocparty/model/user_model.dart';
+import 'package:blocparty/model/login_model/user_model.dart';
 import 'package:blocparty/model/profile_model.dart';
 
 class CreateProfileView extends StatefulWidget {
