7c27d75fe3e0bfab3a0cb785a2c7675b83e63501
Carson Scheich <146121287+CarsonScheich@users.noreply.github.com>
2025-11-09 13:17:44 -0600

    Stashing nothing

--- lib/model/profile_model.dart
+++ lib/model/profile_model.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:blocparty/model/login_model/user_model.dart';
