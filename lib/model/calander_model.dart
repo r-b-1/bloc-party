de2be16a3b384c73f7b806e49f618919a7c733fc
Carson Scheich <146121287+CarsonScheich@users.noreply.github.com>
2025-11-17 10:15:55 -0600

    stashing

--- lib/model/calander_model.dart
+++ lib/model/calander_model.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-List<Appointment> makeAppointments(int hours, int days) {
+List<Appointment> makeAppointments(int hours, int days, String sub, String notes) {
   List<Appointment> date = <Appointment>[];
   final DateTime day = DateTime.now();
   final DateTime startTime = DateTime(day.year, day.month, day.day, 9,0,0);
+  final String subject = sub;
+  final String note = notes;
   DateTime endTime = startTime.add(Duration(
     days: days,
     hours: hours,
@@ -15,15 +16,35 @@ List<Appointment> makeAppointments(int hours, int days) {
   date.add(Appointment(
     startTime: startTime,
      endTime: endTime,
-     subject: '',
+     subject: subject,
      color: Colors.blue,
+     notes: note,
      ));
 
      return date;
 }
-
 class DateScheduled extends CalendarDataSource {
-  DateScheduled(List<Appointment> source){
+  DateScheduled(List<Appointment> source) {
     appointments = source;
+  }
+}
+
+class ScheduleView extends StatelessWidget {
+  const ScheduleView({Key? key}) : super(key: key);
+
+    @override
+  Widget build(BuildContext context) {
+    final dataSource = DateScheduled(
+      makeAppointments(2, 0, "Test Appointment", "This is a sample note"),
+    );
+        return Scaffold(
+      appBar: AppBar(
+        title: const Text('Schedule'),
+      ),
+      body: SfCalendar(
+        view: CalendarView.month,
+        dataSource: dataSource,
+      ),
+    );
   }
 }
--- lib/view/schedule_view.dart
+++ lib/view/schedule_view.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:blocparty/model/calander_model.dart';
 
 class ScheduleView extends StatelessWidget {
   const ScheduleView({super.key});
 
   @override
   Widget build(BuildContext context) {
+        final dataSource = DateScheduled(
+      makeAppointments(2, 0, "Test Appointment", "This is a sample note"),
+    );
     return Scaffold(
       appBar: AppBar(title: const Text('Schedule')),
       body: Container(
         child: SfCalendar(
           view: CalendarView.month,
-          
+                    dataSource: dataSource,
         ),
       ),
     );
